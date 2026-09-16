0ee033d516ad8ad812d6dee68229d87ad98fc572
Muhammad Altaf <zmaltaf99@gmail.com>
2026-09-16 18:30:08 +0500

    complete  assignment

--- login.js
+++ login.js
@@ -4,30 +4,27 @@ const bcrypt = require("bcrypt");
 const filePath = "./users.json";
 
 async function login(email, password) {
+    
 
-    // Users file read karo
     const data = fs.readFileSync(filePath, "utf-8");
 
     const users = JSON.parse(data);
 
-    // Email se user find karo
     const user = users.find(user => user.email === email);
 
-    // Agar user nahi mila
     if (!user) {
         return {
+            
             success: false,
             message: "Invalid email or password"
         };
     }
 
-    // Password compare karo
     const passwordMatch = await bcrypt.compare(
         password,
         user.password
     );
 
-    // Password wrong hai
     if (!passwordMatch) {
         return {
             success: false,
@@ -35,7 +32,6 @@ async function login(email, password) {
         };
     }
 
-    // Login successful
     return {
         success: true,
         message: "Login successful"
