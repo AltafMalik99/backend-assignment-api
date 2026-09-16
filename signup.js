eacc5a8c054e25ac7f20c58b226f56e450951be6
Muhammad Altaf <zmaltaf99@gmail.com>
2026-09-16 18:35:21 +0500

    assignment complete

--- signup.js
+++ signup.js
@@ -5,34 +5,28 @@ const filePath = "./users.json";
 
 async function signup(email, password) {
 
-    // users.json read karo
     const data = fs.readFileSync(filePath, "utf-8");
     const users = JSON.parse(data);
 
-    // Sirf same email check karo
     const existingUser = users.find(user => user.email === email);
 
     if (existingUser) {
         return {
             success: false,
-            message: "Email already exists"
+            message: "Email already exist"
         };
     }
 
-    // Password hash karo
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Naya user
     const newUser = {
         id: Date.now(),
         email: email,
         password: hashedPassword
     };
 
-    // Purane users ko rakho + naya user add karo
     users.push(newUser);
 
-    // File mein save karo
     fs.writeFileSync(
         filePath,
         JSON.stringify(users, null, 2)
